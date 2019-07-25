ca4dc45ff8c51c019ebf5e553584405bd02761b2
ProfesorCIFP <cjgardia.cifp@gmail.com>
2019-07-25 19:03:48 +0100

    El Button "Registrar" atiende a las pulsaciones añadiendole un OnClickListener al Button, mediante código.

---
+++ app/src/main/java/com/example/a07controlesbsicosui/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.a07controlesbsicosui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textResultado = null;
+    private Button btnRegistrar = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textResultado = findViewById(R.id.textResultado);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Registrando....", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+}
