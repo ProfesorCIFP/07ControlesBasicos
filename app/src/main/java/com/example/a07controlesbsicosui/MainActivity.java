57aafc23a8f4ea1f4954d2bc2c9969c5843e22f0
ProfesorCIFP <cjgardia.cifp@gmail.com>
2019-07-25 19:43:29 +0100

    Se añaden varios EditText (Plain, EmailText, PassText), y cuando se pulsa un botón se recogen los datos introducidos y se muestran en el textResultado.

--- app/src/main/java/com/example/a07controlesbsicosui/MainActivity.java
+++ app/src/main/java/com/example/a07controlesbsicosui/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textResultado = null;
+    private EditText etNombre = null;
+    private EditText etEmail = null;
+    private EditText etPassword = null;
     private Button btnRegistrar = null;
 
 
@@ -20,28 +24,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textResultado = findViewById(R.id.textResultado);
+        etNombre = findViewById(R.id.etNombre);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
         btnRegistrar = findViewById(R.id.btnRegistrar);
 
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textResultado.setText("Registrando....");
-
+                String datos = recogeDatosInsertados();
+                textResultado.setText("-- Registrando -- \n" + datos);
             }
         });
     }
 
 
     public void onClick(View view) {
-
+        String datos;
         switch (view.getId()) {
             case R.id.btnLogin:
-                textResultado.setText("Login....");
+                datos = recogeDatosInsertados();
+                textResultado.setText("-- Login -- \n" + datos);
                 break;
 
             case R.id.btnCancelar:
-                textResultado.setText("Cancelando....");
+                datos = recogeDatosInsertados();
+                textResultado.setText("-- Cancelando -- \n" + datos);
                 break;
         }
+    }
+
+    private String recogeDatosInsertados() {
+        String datos = "";
+
+        datos += "Nombre: " + etNombre.getText() + "\n";
+        datos += "Email: " + etEmail.getText() + "\n";
+        datos += "Password: " + etPassword.getText() + "\n";
+
+        return datos;
     }
 }
